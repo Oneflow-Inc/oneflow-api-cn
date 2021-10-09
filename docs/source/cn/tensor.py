5437d8ab62c4ab12a5eb1c39a050799e55fb6099
HENGRui6 <zbx20010202@gmail.com>
2021-10-09 17:27:33 +0800

    fix

--- docs/source/cn/tensor.py
+++ docs/source/cn/tensor.py
@@ -11,12 +11,11 @@ reset_docstr(
         - **data**: 张量的初始数据。可以是列表、元组、NumPy ndarray、标量或张量。
 
     关键词参数：
-        - **dtype** (oneflow.dtype, 可选)： 返回张量的所需数据类型。
-            默认值：如果没有，则从数据推断数据类型。
-        - **device** (oneflow.device, 可选)： 返回张量的所需设备。如果placement 和sbp 为None，则使用当前cpu。
-        - **placement** (oneflow.placement, 可选)： 返回张量的理想位置。
-        - **sbp** (oneflow.sbp or tuple of oneflow.sbp, 可选)： 返回张量的所需 sbp。
-        - **requires_grad **(bool, 可选)： 如果已经自动求导则记录对返回张量的操作。默认值：False
+        - **dtype** (oneflow.dtype, 可选)：返回张量的所需数据类型。默认值：如果没有，则从数据推断数据类型。
+        - **device** (oneflow.device, 可选)：返回张量的所需设备。如果placement 和sbp 为None，则使用当前cpu。
+        - **placement** (oneflow.placement, 可选)：返回张量的理想位置。
+        - **sbp** (oneflow.sbp or tuple of oneflow.sbp, 可选)：返回张量的所需 sbp。
+        - **requires_grad** (bool, 可选)：如果已经自动求导则记录对返回张量的操作。默认值：False。
 
     Noted:
         关键词参数与placement 和sbp 是互斥的。
