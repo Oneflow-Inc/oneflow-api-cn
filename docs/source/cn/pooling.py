d5d16c722d7c5d3d8cbc77d548a55948975b5627
HENGRui6 <zbx20010202@gmail.com>
2021-11-04 18:34:03 +0800

    new translation work on 1104

--- docs/source/cn/pooling.py
+++ docs/source/cn/pooling.py
@@ -153,3 +153,135 @@ reset_docstr(
 
     """
     )   
+
+reset_docstr(
+    oneflow.nn.AvgPool1d,
+    r"""AvgPool1d(kernel_size, stride=None, padding=0, ceil_mode=False, count_include_pad=True)
+
+    在由多个平面组成的输入信号上执行 1D 平均池化。
+    在最简单的情况下，输出值是输入大小为 :math:`(N, C, H, W)` 的层。
+    输出 :math:`(N, C, H_{out}, W_{out})` 和 `kernel_size` ， :math:`k` 可以被精确地描述为：
+    
+    .. math::
+        out(N_i, C_j, l)  = \frac{1}{k} \sum_{m=0}^{k-1}
+                            input(N_i, C_j, stride[0] \times h + m, stride*l + m)
+    
+    如果 :attr:`padding` 非零，则输入在两侧隐式填充 0 以填充点数。
+    参数 :attr:`kernel_size` 、 :attr:`stride` 、 :attr:`padding` 可以为 int 或者单元素元组。
+    
+    Note:
+        当 :attr:`ceil_mode` 为 True 时，如果滑动窗口在 left padding 或输入内开始，则允许滑动窗口出界。
+        忽略在右侧填充区域开始的滑动窗口。
+    
+    参数：
+        - **kernel_size** (Union[int, Tuple[int, int]]): 窗口的大小
+        - **strides** (Union[int, Tuple[int, int]], 可选): 窗口的 stride 。默认值为 None 
+        - **padding** (Union[int, Tuple[int, int]]): 如果非 0 ，在两侧添加隐式填充 0 。默认为 0 
+        - **ceil_mode** (bool): 如果为 True ，将使用 ceil 而不是 floor 来计算输出形状。默认为 False 
+        - **count_include_pad** (bool): 如果为 True ，将在平均计算中填充 0 ，默认为 True 
+    
+    示例：
+
+    .. code-block:: python 
+        
+        import oneflow as flow 
+
+        m = flow.nn.AvgPool1d(kernel_size=3, padding=1, stride=1)
+        x = flow.randn(1, 4, 4)
+        y = m(x)
+        y.shape 
+        oneflow.Size([1, 4, 4])
+
+    """
+)
+
+reset_docstr(
+    oneflow.nn.AvgPool2d,
+    r"""AvgPool2d(kernel_size, stride=None, padding=0, ceil_mode=False, count_include_pad=True, divisor_override=0)
+    
+    在由多个平面组成的输入信号上执行 2D 平均池化。
+
+    在最简单的情况下，输出值是输入大小为 :math:`(N, C, H, W)` 的层。
+    输出 :math:`(N, C, H_{out}, W_{out})` 和 `kernel_size` ， :math:`(kH, kW)` 可以被精确地描述为：
+
+    .. math::
+
+        out(N_i, C_j, h, w)  = \frac{1}{kH * kW} \sum_{m=0}^{kH-1} \sum_{n=0}^{kW-1}
+                               input(N_i, C_j, stride[0] \times h + m, stride[1] \times w + n)
+
+    参数：
+        - **kernel_size** (Union[int, Tuple[int, int]]): 整数或长度为 1 或 2 的整数列表。输入张量的每个维度的窗口大小
+        - **strides** (Union[int, Tuple[int, int]]): 整数或长度为 1 或 2 的整数列表。输入张量的每个维度的滑动窗口的 stride 。默认为 None
+        - **padding** (Tuple[int, int]): 整数或长度为 1 或 2 的整数列表。在两侧添加隐式填充 0 。默认为 0 
+        - **ceil_mode** (bool, default to False): 如果为 True 。将使用 ceil 而不是 floor 来计算输出形状。默认为 False 
+
+    示例：
+
+    .. code-block:: python
+
+        import oneflow as flow 
+
+        m = flow.nn.AvgPool2d(kernel_size=3, padding=1, stride=1)
+        x = flow.randn(1, 4, 4, 4)
+        y = m(x)   
+        y.shape
+        oneflow.Size([1, 4, 4, 4])
+
+    """
+)
+
+reset_docstr(
+    oneflow.nn.AvgPool3d,
+    r"""AvgPool3d(kernel_size, stride=None, padding=0, ceil_mode=False, count_include_pad=True, divisor_override=0)
+
+    在由多个平面组成的输入信号上执行 3D 平均池化。
+    在最简单的情况下，输出值是输入大小为 :math:`(N, C, D, H, W)` 的层。
+    输出 :math:`(N, C, D_{out}, H_{out}, W_{out})` 和 `kernel_size` ， :math:`(kD, kH, kW)` 可以被精确地描述为：
+    
+    .. math::
+        out(N_i, C_j, d, h, w)  = \frac{1}{kD * kH * kW } \sum_{k=0}^{kD-1} \sum_{m=0}^{kH-1} \sum_{n=0}^{kW-1}
+                               input(N_i, C_j, stride[0] \times d + k, stride[1] \times h + m, stride[2] \times w + n)
+    
+    如果 :attr:`padding` 非零，则输入在三侧隐式填充 0 以填充点数。
+    
+    Note:
+
+        当 :attr:`ceil_mode` 为 True 时，如果滑动窗口在 left padding 或输入内开始，则允许滑动窗口出界。
+        忽略在右侧填充区域开始的滑动窗口。
+    
+    参数：
+        - **kernel_size** (Union[int, Tuple[int, int, int]]): 窗口的大小
+        - **strides** (Union[int, Tuple[int, int, int]], 可选): 窗口的 stride 。默认值为 None 
+        - **padding** (Union[int, Tuple[int, int, int]]):  如果非 0 ，在三侧添加隐式填充 0 。默认为 0 
+        - **ceil_mode** (bool): 如果为 True ，将使用 ceil 而不是 floor 来计算输出形状。默认为 False 
+        - **count_include_pad** (bool): 如果为 True ，将在平均计算中填充 0 ，默认为 True 
+        - **divisor_override** (int): 如果设定了 attr:`divisor_override` ，它将用作除数，否则 attr:`kernel_size` 将作为除数。默认为 0 
+    
+    形状：
+        - Input: :math:`(N, C, D_{in}, H_{in}, W_{in})`
+
+        - Output: :math:`(N, C, D_{out}, H_{out}, W_{out})` 
+        
+          .. math::
+              D_{out} = \left\lfloor\frac{D_{in} + 2 \times \text{padding}[0] - \text{kernel_size}[0]}{\text{stride}[0]} + 1\right\rfloor
+        
+          .. math::
+              H_{out} = \left\lfloor\frac{H_{in} + 2 \times \text{padding}[1] - \text{kernel_size}[1]}{\text{stride}[1]} + 1\right\rfloor
+        
+          .. math::
+              W_{out} = \left\lfloor\frac{W_{in} + 2 \times \text{padding}[2] - \text{kernel_size}[2]}{\text{stride}[2]} + 1\right\rfloor
+
+    示例：
+    
+    .. code-block:: python
+    
+        import oneflow as flow
+        
+        m = flow.nn.AvgPool3d(kernel_size=(2,2,2),padding=(0,0,0),stride=(1,1,1))
+        x = flow.randn(9, 7, 11, 32, 20)
+        y = m(x)
+        y.shape
+        oneflow.Size([9, 7, 10, 31, 19])
+
+    """
+)
--- docs/source/cn/tensor_ops.py
+++ docs/source/cn/tensor_ops.py
@@ -75,3 +75,168 @@ reset_docstr(
     """
 )
 
+reset_docstr(
+    oneflow.Tensor.cpu,
+    r"""
+    返回此对象在 CPU 内存中的副本。
+    如果此对象已在 CPU 内存中且位于正确的设备上，则不会执行复制并返回原始对象。
+
+    示例:
+
+    .. code-block:: python
+
+        >>> import oneflow as flow
+        
+        >>> input = flow.tensor([1, 2, 3, 4, 5], device=flow.device("cuda"))
+        >>> output = input.cpu()
+        >>> output.device
+        device(type='cpu', index=0)
+    """
+)
+
+reset_docstr(
+    oneflow.Tensor.cuda,
+    r"""cuda(device=None)
+
+    返回此对象在 CUDA 内存中的副本。
+
+    如果此对象已在 CUDA 内存中且位于正确的设备上，则不会执行复制并返回原始对象。
+
+    参数：
+        - **device**  (flow.device): 目标 GPU 设备。默认为当前 CUDA 设备。
+
+    示例：
+
+    .. code-block:: python
+
+        >>> import oneflow as flow
+        
+        >>> input = flow.Tensor([1, 2, 3, 4, 5])
+        >>> output = input.cuda()
+        >>> output.device
+        device(type='cuda', index=0)
+
+    """
+)
+
+reset_docstr(
+    oneflow.Tensor.double,
+    r"""
+    
+    `Tensor.double()` 等价于 `Tensor.to(flow.float64)` 。 参见： :mod:`oneflow.to` 。
+    
+    参数：
+        - **input**  (Tensor): 输入张量
+
+    示例：
+
+    .. code-block:: python
+
+        >>> import oneflow as flow
+        
+        >>> input = flow.randint(0, 5, (3,3))
+        >>> input = input.double()
+        >>> input.dtype
+        oneflow.float64
+    """
+)
+
+reset_docstr(
+    oneflow.Tensor.float,
+    r"""float(input)
+    
+    `Tensor.float()` 等价于 `Tensor.to(flow.float32)` 。 参见： :mod:`oneflow.to` 。
+
+    参数：
+        - **input** (Tensor): 输入张量
+
+    示例：
+
+    .. code-block:: python
+
+        >>> import oneflow as flow
+        
+        >>> input = flow.randint(0, 5, (3,3))
+        >>> input = input.float()
+        >>> input.dtype
+        oneflow.float32
+    """
+)
+
+reset_docstr(
+    oneflow.gather,
+    r"""gather(dim, index, sparse_grad=False)
+
+    沿由 :attr:`dim` 指定的维度收集值。
+
+    对 3-D tensor ，输出被定义为::
+
+        out[i][j][k] = input[index[i][j][k]][j][k]  # 如果 dim == 0
+        out[i][j][k] = input[i][index[i][j][k]][k]  # 如果 dim == 1
+        out[i][j][k] = input[i][j][index[i][j][k]]  # 如果 dim == 2
+
+    :attr:`input` 和 :attr:`index` 的维度数必须相同。对于所有 ``d != dim`` 的维度 d ，
+    必须有 ``index.size(d) <= input.size(d)`` ， :attr:`out` 的形状和 :attr:`index` 的形状相同。
+    请注意， ``input`` 和 ``index`` 不会相互广播。
+
+    参数：
+        - **input** (Tensor): 源张量
+        - **dim** (int): 索引的维度
+        - **index** (LongTensor): 要收集的元素的索引
+
+    示例：
+
+    .. code-block:: python
+
+        >>> import oneflow as flow
+
+        >>> input = flow.randn(3, 4, 3, 5, dtype=flow.float32)
+        >>> index = flow.randint(0, 3, (3, 4, 3, 5))
+        >>> output = flow.gather(input, 1, index)
+        >>> output.shape
+        oneflow.Size([3, 4, 3, 5])
+    """
+)
+
+reset_docstr(
+    oneflow.Tensor.int, 
+    r"""
+    `Tensor.int()` 等价于 `Tensor.to(flow.int32)` 。 参见： :mod:`oneflow.to` 。
+
+    参数：
+        - **input** (Tensor): 输入张量
+
+    示例：
+
+    .. code-block:: python
+
+        >>> import oneflow as flow
+        
+        >>> input = flow.randn(1, 2, 3, dtype=flow.float32)
+        >>> input = input.int()
+        >>> input.dtype
+        oneflow.int32
+    """
+)
+
+reset_docstr(
+    oneflow.Tensor.item,
+    r"""
+    将 tensor 的值作为标准 Python 数字返回。仅适用于只有一个元素的 tensor 。
+    其他情况请参考 :mod:`oneflow.tolist` 。
+
+    这个操作不可导。
+
+    参数：
+        - **input** (Tensor): 输入张量
+
+    示例：
+
+    .. code-block:: python
+
+        >>> import oneflow as flow
+        >>> x = flow.tensor([1.0])
+        >>> x.item()
+        1.0
+    """
+)
