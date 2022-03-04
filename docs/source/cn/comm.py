ca9f9818eb3f8d0c9285e959787e11324eb5aa06
ChenhaoWang2000 <wangchenhao.jy@gmail.com>
2022-03-04 16:08:23 +0800

    将 comm.py 中的 docstring 不按照 doctest 的约定写，当作纯文本写

--- docs/source/cn/comm.py
+++ docs/source/cn/comm.py
@@ -49,27 +49,27 @@ reset_docstr(
 
     .. code-block:: python
 
-        >>> # 我们有一个进程组，包含两个 rank。
-        >>> import oneflow as flow
+        # 我们有一个进程组，包含两个 rank。
+        > import oneflow as flow
 
-        >>> input = flow.tensor([[1, 2], [3, 4]], device="cuda") + flow.env.get_local_rank()
-        >>> # rank0 的输入 +ONLY_CHECK_RANK_0
-        >>> input
+        > input = flow.tensor([[1, 2], [3, 4]], device="cuda") + flow.env.get_local_rank()
+        # rank0 的输入
+        > input
         tensor([[1, 2],
                 [3, 4]], device='cuda:0', dtype=oneflow.int64)
-        >>> # rank1 的输入 +ONLY_CHECK_RANK_1
-        >>> input
+        # rank1 的输入
+        > input
         tensor([[2, 3],
                 [4, 5]], device='cuda:1', dtype=oneflow.int64)
-        >>> tensor_list = [flow.zeros(2, 2, dtype=flow.int64) for _ in range(2)]
-        >>> flow.comm.all_gather(tensor_list, input)
-        >>> # rank0 的结果 +ONLY_CHECK_RANK_0
-        >>> tensor_list
+        > tensor_list = [flow.zeros(2, 2, dtype=flow.int64) for _ in range(2)]
+        > flow.comm.all_gather(tensor_list, input)
+        # rank0 的结果
+        > tensor_list
         [tensor([[1, 2],
                 [3, 4]], device='cuda:0', dtype=oneflow.int64), tensor([[2, 3],
                 [4, 5]], device='cuda:0', dtype=oneflow.int64)]
-        >>> # rank1 的结果 +ONLY_CHECK_RANK_1
-        >>> tensor_list
+        # rank1 的结果
+        > tensor_list
         [tensor([[1, 2],
                 [3, 4]], device='cuda:1', dtype=oneflow.int64), tensor([[2, 3],
                 [4, 5]], device='cuda:1', dtype=oneflow.int64)]
