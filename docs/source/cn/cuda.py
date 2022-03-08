d8fce9a533157a19e5d4acc1c82ecafb4ce4418e
lizhaoyang <s882li@uwaterloo.ca>
2022-03-08 10:18:26 +0800

    translate

---
+++ docs/source/cn/cuda.py
@@ -0,0 +1,71 @@
+import oneflow
+from docreset import reset_docstr
+
+reset_docstr(
+    oneflow.cuda.BoolTensor,
+    r"""
+    创造一个 bool 类型张量，其和 :func:`oneflow.Tensor` 共享相同的参数。
+    """
+)
+
+reset_docstr(
+    oneflow.cuda.ByteTensor,
+    r"""
+    创造一个 uint8 类型张量，其和 :func:`oneflow.Tensor` 共享相同的参数。
+    """
+)
+
+reset_docstr(
+    oneflow.cuda.CharTensor,
+    r"""
+    创造一个 int8 类型张量，其和 :func:`oneflow.Tensor` 共享相同的参数。
+    """
+)
+
+reset_docstr(
+    oneflow.cuda.DoubleTensor,
+    r"""
+    创造一个 float64 类型张量，其和 :func:`oneflow.Tensor` 共享相同的参数。
+    """
+)
+
+reset_docstr(
+    oneflow.cuda.FloatTensor,
+    r"""
+    创造一个 float32 类型张量，其和 :func:`oneflow.Tensor` 共享相同的参数。
+    """
+)
+
+reset_docstr(
+    oneflow.cuda.HalfTensor,
+    r"""
+    创造一个 float16 类型张量，其和 :func:`oneflow.Tensor` 共享相同的参数。
+    """
+)
+reset_docstr(
+    oneflow.cuda.IntTensor,
+    r"""
+    创造一个 int32 类型张量，其和 :func:`oneflow.Tensor` 共享相同的参数。
+    """
+)
+
+reset_docstr(
+    oneflow.cuda.LongTensor,
+    r"""
+    创造一个 int64 类型张量，其和 :func:`oneflow.Tensor` 共享相同的参数。
+    """
+)
+
+reset_docstr(
+    oneflow.cuda.device_count,
+    r"""
+    返回可用的 GPU 数量。
+    """
+)
+
+reset_docstr(
+    oneflow.cuda.is_available,
+    r"""
+    返回一个 bool 用于指代 CUDA 是否可用。
+    """
+)
--- docs/source/cn/env.py
+++ docs/source/cn/env.py
@@ -2,12 +2,44 @@ import oneflow
 from docreset import reset_docstr
 
 reset_docstr(
-    oneflow.env.get_world_size,
+    oneflow.env.get_node_size,
     r"""
     返回当前进程组包含的机器数量。
 
     返回值：
         进程组中包含的机器数量。
+    
+    """
+)
+
+reset_docstr(
+    oneflow.env.get_local_rank,
+    r"""返回当前机器的本地 rank 。本地 rank 在全局上不是独一的，其仅对于一个机器的单个进程是独一的。
+
+    返回值：
+        当前机器进程的本地 rank 。
+
+    """
+)
+
+reset_docstr(
+    oneflow.env.get_world_size,
+    r"""
+    返回当前进程组包含的进程数量。
+
+    返回值：
+        进程组中包含的进程数量。
+    
+    """
+)
+
+reset_docstr(
+    oneflow.env.is_multi_client,
+    r"""
+    返回当前是否处于多用户模式。
+
+    返回值：
+        如果当前处于多用户模式则为真，否则为假。
     
     """
 )
