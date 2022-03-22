170acd5688253216dbf12d459c38119d794234d5
Zhaoyang Li <92694420+S882li@users.noreply.github.com>
2022-03-21 23:12:29 -0400

    Update docs/source/cn/nn_init.py
    Co-authored-by: Yiqing zhang <96572794+rainyq69@users.noreply.github.com>

--- docs/source/cn/nn_init.py
+++ docs/source/cn/nn_init.py
@@ -72,7 +72,7 @@ reset_docstr(
     参数：
         - **tensor** - 一个 n 维的 `flow.Tensor`
         - **a** - 此层之后的激活层使用的负梯度（只能用于 ``'leaky_relu'`` ）
-        - **mode** - ``'fan_in'`` (默认) 或 ``'fan_out'`` 中的一个。选择 ``'fan_in'`` 将保留正向过程中的的权重方差。选择 ``'fan_out'`` 将保留反向过程中的权重方差
+        - **mode** - ``'fan_in'`` (默认) 或 ``'fan_out'`` 中的一个。选择 ``'fan_in'`` 将保留前向传播的权重方差。选择 ``'fan_out'`` 将保留反向传播的权重方差
         - **nonlinearity** - 非线性函数 (`nn.functional` 的名称),
             建议仅使用 ``'relu'`` 或者 ``'leaky_relu'`` (默认)
 
