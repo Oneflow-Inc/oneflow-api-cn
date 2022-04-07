8c37581e07ee8b5997ebf608b19aa7b5ebf9ff01
Tingyu Guo <69283456+robin978@users.noreply.github.com>
2022-04-07 10:50:16 +0800

    Apply suggestions from code review

--- docs/source/cn/conv.py
+++ docs/source/cn/conv.py
@@ -37,9 +37,9 @@ reset_docstr(
     请参阅 :class:`~oneflow.nn.Conv3d` 获取有关详细信息和输出形状。
 
     参数：
-        - **input** - 形状的量化输入张量 :math:`(\text{minibatch} , \text{in_channels} , iD , iH , iW)`
-        - **weight** - 形状的量化滤波器 :math:`(\text{out_channels} , \frac{\text{in_channels}}{\text{groups}} , kD , kH , kW)`
-        - **bias** - 非量化的形状的偏置张量 :math:`(\text{out_channels})`。张量类型必须为 `flow.float`。
+        - **input** - 形状为 :math:`(\text{minibatch} , \text{in_channels} , iD , iH , iW)` 的量化输入张量
+        - **weight** - 形状为 :math:`(\text{out_channels} , \frac{\text{in_channels}}{\text{groups}} , kD , kH , kW)` 的量化滤波器
+        - **bias** - 形状为 :math:`(\text{out_channels})` 的化偏置张量，其类型必须为 `flow.float`。
         - **stride** - 卷积核的步长。可以是单个数字或元组 `(sD, sH, sW)`， 默认值：1。
         - **padding** - 输入两侧的隐式填充。可以是单个数字或元组 `(padD, padH, padW)`，默认值：0。
         - **dilation** - 内核元素之间的间距。可以是单个数字或元组 `(dD, dH, dW)`，默认值：1。
