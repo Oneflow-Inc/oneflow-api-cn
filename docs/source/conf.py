ecac0d13c658a2bb5eb11e3bbab3b92401329130
YaoChi <later@usopp.net>
2022-04-11 23:19:12 +0800

    refine mathjax cdn

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -192,6 +192,7 @@ autodoc_default_options = {
     "exclude-members": "forward",
 }
 
+mathjax_path = "https://cdn.bootcdn.net/ajax/libs/mathjax/2.7.7/latest.js?config=TeX-AMS-MML_HTMLorMML"
 
 def should_skip_member(app, what, name, obj, skip, options):
     import collections
