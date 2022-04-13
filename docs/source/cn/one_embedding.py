53c9124ef2dd8d1310c21233961a6d1155c95f06
Tingyu Guo <69283456+robin978@users.noreply.github.com>
2022-04-13 12:59:48 +0800

    Apply suggestions from code review
    Co-authored-by: Yiqing zhang <96572794+rainyq69@users.noreply.github.com>

--- docs/source/cn/one_embedding.py
+++ docs/source/cn/one_embedding.py
@@ -3,16 +3,16 @@ from docreset import reset_docstr
 
 reset_docstr(
     oneflow.one_embedding.MultiTableEmbedding,
-    r"""MultiTableEmbedding 代表具有相同 embedding_dim、dtype 和 key_type 的多个 embedding 表。
+    r"""MultiTableEmbedding 表示具有相同 embedding_dim、dtype 和 key_type 的多个 embedding 表。
 
     参数：
-        - **name** (str) - Embedding 的名字。
+        - **name** (str) - 实例化的 Embedding 层的名字。
         - **embedding_dim** (int) - 每一个 embedding 向量的大小。
         - **dtype** (flow.dtype) - embedding 的数据类型。
         - **key_type** (flow.dtype) - 特征 ID 的数据类型。
         - **tables** (list) - 由 flow.one_embedding.make_table_options 生成的列表中的表格参数。
         - **store_options** (dict) - Embedding 的存储选项。
-        - **default_initializer** (dict, optional) - 如果表参数为 None，使用 default_initializer 来初始化表，默认为 None。
+        - **default_initializer** (dict, optional) - 如果参数 tables 为 None，则使用 default_initializer 来初始化表。默认为 None。
     
     示例：
 
@@ -21,7 +21,7 @@ reset_docstr(
         > import oneflow as flow
         > import numpy as np
         > import oneflow.nn as nn
-        > # 有 3 个表的样例
+        > # 一个使用三个表的简单样例
         > table_size_array = [39884407, 39043, 17289]
         > vocab_size = sum(table_size_array)
         > num_tables = len(table_size_array)
@@ -84,7 +84,7 @@ reset_docstr(
     """保存快照。
 
     参数：       
-        - **snapshot_name** (str) - snapshot_name，快照将被保存在 your_configed_persistent_path 目录下。
+        - **snapshot_name** (str) - 快照的名称。快照将被保存在路径 your_configed_persistent_path 下的 snapshots 目录中。
     
     示例：
 
@@ -103,7 +103,7 @@ reset_docstr(
     """加载快照。
 
         参数：
-            - **snapshot_name** (str) - snapshot_name，快照将从 your_configed_persistent_path 下加载。
+            - **snapshot_name** (str) - 快照的名称。快照将从路径 your_configed_persistent_path 下加载。
     
         示例：
 
@@ -121,8 +121,8 @@ reset_docstr(
     """MultiTableEmbedding 的前向计算。
 
         参数：
-            - **ids** (flow.tensor) - 特征的id。
-            - **table_ids** (flow.tensor, optional) - 每个 table_id 的 id 必须与 ids 的形状相同。如果只配置了一个表或者 id 的形状为（batch_size, num_tables），并且每个列的 id 都属于表的 column_id，就没有必要传递 table_ids，否则，应该传递 tensor_ids。
+            - **ids** (flow.tensor) - 特征 id。
+            - **table_ids** (flow.tensor, optional) - 每个 id 的 table_id 必须与 ids 的形状相同。如果只配置了一个表或者 id 的形状为（batch_size, num_tables），并且每个列的 id 都属于表的 column_id，就无需传递 table_ids，否则，应该传递 tensor_ids。
             
         返回：
             - **flow.tensor** - embedding 查询的结果。
