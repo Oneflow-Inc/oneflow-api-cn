56c7b1fce337e5518d02c20aed7be66c2d1876a4
ChenQiaoling <48576019+Chenqll@users.noreply.github.com>
2022-05-12 22:39:33 +0800

    Update docs/source/cn/is_tensor.py
    Co-authored-by: Tingyu Guo <69283456+robin978@users.noreply.github.com>

--- docs/source/cn/is_tensor.py
+++ docs/source/cn/is_tensor.py
@@ -6,7 +6,7 @@ reset_docstr(
     r"""
     is_tensor(input) -> (bool)
 
-    注意，这个函数只是在做 ``isinstance(obj, Tensor)`` 。使用 ``isinstance`` 检查对 mypy 的类型检查更好，而且更明确--所以建议使用它而不是 ``is_tensor`` 。
+    注意，这个函数只是在执行 ``isinstance(obj, Tensor)`` 。使用 ``isinstance`` 检查对 mypy 的类型更明确，所以建议使用该函数而不是 ``is_tensor`` 。
     
     参数：
         obj (Object): 被测试的对象。
