def8e906965ffef0f60439a9828c2f5f70c997a2
ChenQiaoling <48576019+Chenqll@users.noreply.github.com>
2022-05-13 13:05:11 +0800

    Update docs/source/cn/tensor_str.py
    Co-authored-by: Tingyu Guo <69283456+robin978@users.noreply.github.com>

--- docs/source/cn/tensor_str.py
+++ docs/source/cn/tensor_str.py
@@ -11,7 +11,7 @@ reset_docstr(
         - **threshold** - 触发总结的数组元素的总数而不是完全 ``repr`` （默认值=1000）。
         - **edgeitems** - 在每个维度的开始和结束时，汇总的数组的数量（默认值=3）。
         - **linewidth** - 每行插入换行符的字符数（默认 =  terminal_columns ）。
-        - **profile** - 默认为漂亮的打印。可以用上述任何一个选项来覆盖。(`default`, `short`, `full` 中的任何一个)
+        - **profile** - 默认为  pretty printing，可以用上述任何一个选项来覆盖。(`default`, `short`, `full` 中的任何一个)
         - **sci_mode** - 启用（True）或禁用（False）科学计数法。如果 None（默认）被指定，其值由 `oneflow._tensor_str._Formatter` 定义。这个值是由框架自动选择的。
 
     .. note::
